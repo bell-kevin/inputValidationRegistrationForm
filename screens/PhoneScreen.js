04424af0a62ff1f495c437f494786a3b04c67a99
Kevin Bell <bellKevin@pm.me>
2023-03-16 20:34:46 -0600

    Update PhoneScreen.js

--- screens/PhoneScreen.js
+++ screens/PhoneScreen.js
@@ -1,27 +1,54 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { UserContext } from '../context/userContext';
 
 const PhoneScreen = ({ navigation }) => {
   const userContext = useContext(UserContext);
+  const [phone, setPhone] = useState('');
+  const [showError, setShowError] = useState(false);
 
-  const setPhone = (phone) => {
-    userContext.setPhone(phone);
-  }
+  const handleNextPress = () => {
+    const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+    if (!phoneRegex.test(phone)) {
+      setShowError(true);
+    } else {
+      setShowError(false);
+      userContext.setPhone(phone);
+      navigation.navigate('Email');
+    }
+  };
+
+  const handlePhoneChange = (text) => {
+    setPhone(text);
+    const phoneRegex = /^\d{3}-\d{3}-\d{4}$/;
+    if (!phoneRegex.test(text)) {
+      setShowError(true);
+    } else {
+      setShowError(false);
+    }
+  };
+  
+  
+
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Enter your phone</Text>
       <TextInput
         style={styles.input}
-        placeholder="Phone number"
+        placeholder="801-292-8544"
         keyboardType="numeric"
-        value={userContext.phone}
-        onChangeText={setPhone}
+        value={phone}
+        onChangeText={handlePhoneChange}
       />
+      {showError && (
+        <Text style={styles.errorText}>Please enter a valid phone number in the format XXX-XXX-XXXX</Text>
+
+      )}
       <Button
         title="Next"
-        onPress={() => navigation.navigate('Email')}
+        onPress={handleNextPress}
+        disabled={phone.length !== 12}
       />
     </View>
   );
@@ -51,7 +78,12 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: 'darkgreen',
   },
+  errorText: {
+    color: 'red',
+    marginBottom: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
 
 export default PhoneScreen;
-
